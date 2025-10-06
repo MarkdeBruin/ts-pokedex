27ccff46c4281f8c9ea378ba63bb86a4f2e3b0fc
MarkdeBruin <contact@mrkdbrn.com>
2025-10-06 10:11:49 +0200

    startREPL()

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,33 @@
+import { createInterface } from "node:readline";
+import { stdin, stdout } from "node:process";
+
+export function startREPL() {
+  const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", (line) => {
+    const cleanLine = cleanInput(line);
+    
+    if (cleanLine.length === 0) {
+      rl.prompt();
+      return;
+    }
+    
+    console.log("Your command was:", cleanLine[0]);
+    rl.prompt();
+  });
+  
+  rl.on("SIGINT", () => {
+    console.log("\nExiting Pokedex...");
+    process.exit(0);
+  });
+}
+
 export function cleanInput(input: string): string[] {
   if (!input.trim()) return [];
   return input.trim().toLowerCase().split(/\s+/);
